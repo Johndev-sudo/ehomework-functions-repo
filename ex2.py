ed7bb6d83deb345851260d3787cd3a746e3c29c2
Johndev-sudo <postolacheion18@gmail.com>
2026-01-08 17:34:19 +0200

    Add ex2.py file

---
+++ ex2.py
@@ -0,0 +1,8 @@
+def aria_dreptunghi(latime, lungime):
+    return latime * lungime
+
+latime = 5   # cm
+lungime = 10 # cm
+
+aria = aria_dreptunghi(latime, lungime)
+print("Aria dreptunghiului este:", aria, "cm²")
